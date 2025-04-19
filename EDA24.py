25ab14494e83acebf5c3239498b9ee03c3911b33
danielwxyzu <daniel2023084@gmail.com>
2025-04-19 00:10:00 -0700

    Calculated w/l and wr% for each team and player. Viz'd distributions for wr%. Viz'd best and worst corrs for result col

--- EDA24.py
+++ EDA24.py
@@ -13,15 +13,118 @@ total: 1,137,846
 playerdf = pd.read_csv('Data/playerdata') 
 teamdf = pd.read_csv('Data/teamdata') 
 
-
 # vizualize distribution of data first? plot frequency of the result column (W/L) based on team?
-# use .loc to select count the times a team or player has won 
 
 list_of_teams = teamdf['teamname'].unique()
 list_of_players = playerdf['playername'].unique()
 
-def count_wins(df):
-    w_count = 0
-    l_count = 0
-    for i in df.columns:
-        return
+# initialize df's to find w's and l's
+team_wl_record = pd.DataFrame(columns=['names', 'w', 'l', 'wr%'])
+team_wl_record['names'] = list_of_teams
+team_wl_record['w'] = 0
+team_wl_record['l'] = 0
+team_wl_record['wr%'] = 0.00
+
+player_wl_record = pd.DataFrame(columns=['names', 'w', 'l', 'wr%'])
+player_wl_record['names'] = list_of_players
+player_wl_record['w'] = 0
+player_wl_record['l'] = 0
+player_wl_record['wr%'] = 0.00
+
+# define functions (could probably simplify into just one)
+def calc_team_wr(result_col):
+    for idx in result_col.index:
+        team_name = teamdf.loc[idx, 'teamname']
+        result = teamdf.loc[idx, 'result']
+
+        team_idx = team_wl_record.index[team_wl_record['names'] == team_name].tolist()[0]
+
+        if result == 1:
+            team_wl_record.loc[team_idx, 'w'] += 1
+        else:
+            team_wl_record.loc[team_idx, 'l'] += 1
+
+    team_wl_record['wr%'] = team_wl_record['w']/(team_wl_record['w']+team_wl_record['l'])
+
+def calc_player_wr(result_col):
+    for idx in result_col.index:
+        player_name = playerdf.loc[idx, 'playername']
+        result = playerdf.loc[idx, 'result']
+
+        player_idx = player_wl_record.index[player_wl_record['names'] == player_name].tolist()[0]
+
+        if result == 1:
+            player_wl_record.loc[player_idx, 'w'] += 1
+        else:
+            player_wl_record.loc[player_idx, 'l'] += 1
+
+    player_wl_record['wr%'] = player_wl_record['w']/(player_wl_record['w']+player_wl_record['l'])
+
+calc_team_wr(teamdf['result'])
+calc_player_wr(playerdf['result'])
+'''
+team_wl_record.head()
+player_wl_record.head()
+team_wl_record.describe()
+player_wl_record.describe()
+'''
+
+# check distributions
+team_wl_record['wr%'].plot.hist(bins = 7, alpha = 0.5, density = False)
+plt.ylabel('Frequency')
+plt.xlabel('WR%')
+plt.title('Distribution of Team WR%')
+plt.show()
+player_wl_record['wr%'].plot.hist(bins = 7, alpha = 0.5, density = False)
+plt.ylabel('Frequency')
+plt.xlabel('WR%')
+plt.title('Distribution of Player WR%')
+plt.show()
+
+'''
+plot corr mx of broader df's where target is result
+not using .abs() because a lot of columns are just opposites 
+i.e. barons vs opp_barons which have the same corr but in opposite directions
+'''
+target = 'result'
+top_n = 20
+corr_matrix = teamdf.corr(numeric_only=True)
+
+top_corr = corr_matrix[target].sort_values(ascending=False).head(top_n)
+subset = corr_matrix.loc[top_corr.index, top_corr.index]
+
+plt.figure(figsize=(12,10))
+ax = sns.heatmap(
+    subset,
+    annot=True,
+    cmap='coolwarm',
+    linewidths=0.5
+)
+
+plt.xticks(rotation=45, ha='right', fontsize=10)
+plt.yticks(fontsize=10)
+plt.title(f'{top_n} Correlations w/ {target}')
+plt.tight_layout()
+plt.show()
+
+# bottom correlations
+target = 'result'
+bot_n = 20
+corr_matrix = teamdf.corr(numeric_only=True)
+
+bot_corr = corr_matrix[target].sort_values(ascending=True).head(bot_n)
+subset = corr_matrix.loc[bot_corr.index, bot_corr.index]
+
+plt.figure(figsize=(12,10))
+ax = sns.heatmap(
+    subset,
+    annot=True,
+    cmap='RdBu',
+    linewidths=0.5
+)
+
+plt.xticks(rotation=45, ha='right', fontsize=10)
+plt.yticks(fontsize=10)
+plt.title(f'{bot_n} Correlations w/ {target}')
+plt.tight_layout()
+plt.show()
