43e47feb72d4a38f1c74172ce4daa4ecd3ad5cd6
danielwxyzu <daniel2023084@gmail.com>
2025-04-22 23:04:28 -0700

    Worked on feature engineering

--- FeatureEng.py
+++ FeatureEng.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime
 
 teamdf = pd.read_csv('Data/teamdata')
 
@@ -10,17 +9,44 @@ What features can we add to the df which would provide better insight into w/l p
 - Multi kills metric
 - Objective control metric
 - Gamestate @ 15 or @ 10
+
+Maybe the multi kills metric can get collapsed into a micro game score 
+and the obj control can get collapsed into a macro game score
 '''
 for col in teamdf.columns:
     print(col)
 
-# calculate performance of most recent 5 games
+# calculate rolling average of most recent 5 games
+teamdf['date'] = pd.to_datetime(teamdf['date'])
 
-fivelatestindex = sorted(range(len(teamdf['date'])), key=lambda i: teamdf['date'][i], reverse=True)[:5]
-print(fivelatestindex)
+teamdf['recent_form'] = (
+    teamdf.sort_values(['teamname', 'date'])
+      .groupby('teamname')['result']
+      .transform(lambda x: x.shift(1).rolling(window=5).mean()))
+teamdf['recent_form'] = teamdf['recent_form'].fillna(teamdf['result'])
+
+# multikills metric
+teamdf['weighted_multikill'] = (
+    1 * teamdf['doublekills'] +
+    2 * teamdf['triplekills'] +
+    3 * teamdf['quadrakills'] +
+    4 * teamdf['pentakills']
+)
+
+# objective control metric
+teamdf['weighted_objective_diff'] = (
+    (teamdf['dragons'] - teamdf['opp_dragons']) +
+    (teamdf['elders'] - teamdf['opp_elders']) * 2 +
+    (teamdf['heralds'] - teamdf['opp_heralds']) +
+    (teamdf['barons'] - teamdf['opp_barons']) * 2 +
+    (teamdf['towers'] - teamdf['opp_towers']) +
+    (teamdf['turretplates'] - teamdf['opp_turretplates']) +
+    (teamdf['inhibitors'] - teamdf['opp_inhibitors']) * 2
+)
 
 
 
+# game state at 10/15
 substring = 'at15'
 substring2 = 'at10'
 for col in teamdf.columns:
