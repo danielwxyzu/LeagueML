8a7201e03175f5568cfd80f32ff3a7a82ebfde85
danielwxyzu <daniel2023084@gmail.com>
2025-04-29 20:18:07 -0700

    Exported model into a pkl file

--- logreg.py
+++ logreg.py
@@ -6,12 +6,13 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report
+import joblib
 
 df = pd.read_csv('Data/teamdfwithfeatures')
 for col in df.columns:
     print(col)
 
-X = df.drop(columns=['result', 'leaguetype', 'gameid', 'league',
+#X = df.drop(columns = ['result', 'leaguetype', 'gameid', 'league',
 'split',
 'playoffs',
 'date',
@@ -27,6 +28,10 @@ X = df.drop(columns=['result', 'leaguetype', 'gameid', 'league',
 'ban4',
 'ban5',
 'gamelength'])
+#X = df[['recent_form', 'gamestateat15', 'weighted_objective_diff']]
+#X = df[['recent_form', 'weighted_objective_diff', 'gspd', 'earned gpm']]
+X = df[['recent_form', 'weighted_objective_diff', 'gspd', 'team kpm']]
+#X = df[['recent_form', 'weighted_objective_diff', 'gspd', 'dragonsoul']]
 y = df['result']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -40,9 +45,20 @@ print("ROC AUC:", roc_auc_score(y_test, y_pred_probs))
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 
+'''
+Precision: True Positives / (True Positives + False Positives)
+Recall: True Positives / (True Positives + False Negatives)
+F1 (balance between the above): 2 x (Precision x Recall) / (Precision + Recall)
+Support: # of samples in that class
+Accuracy: Corret Pred / Total Pred
+Macro Avg: Avg of P, R, F1 across classes
+Weighted Avg: Macro Avg weighed by # of samples in each class
+'''
+
 feature_importance = pd.DataFrame({
     'feature': X.columns,
     'coefficient': abs(logreg.coef_[0])
-}).sort_values(by='coefficient', ascending=False)
-
+}).sort_values(by='coefficient', ascending=False).head(10)
 print(feature_importance)
+
+joblib.dump(logreg, 'league_win_predictor.pkl')
